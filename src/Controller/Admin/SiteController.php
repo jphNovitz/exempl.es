232694072e533ec4f6bc41047d0bd36f0d6a72ab
Novitz Jean-Philippe <novitz@gmail.com>
2024-02-02 23:16:52 +0100

    Add: Site Controller
    Admin Site controller for C.R.U.D actions

---
+++ src/Controller/Admin/SiteController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Site;
+use App\Form\SiteType;
+use App\Repository\SiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/site')]
+class SiteController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_site_index', methods: ['GET'])]
+    public function index(SiteRepository $siteRepository): Response
+    {
+        return $this->render('admin/site/index.html.twig', [
+            'sites' => $siteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_site_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $site = new Site();
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($site);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_site_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/site/new.html.twig', [
+            'site' => $site,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_site_show', methods: ['GET'])]
+    public function show(Site $site): Response
+    {
+        return $this->render('admin/site/show.html.twig', [
+            'site' => $site,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_site_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Site $site, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_site_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/site/edit.html.twig', [
+            'site' => $site,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_site_delete', methods: ['POST'])]
+    public function delete(Request $request, Site $site, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$site->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($site);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_site_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
