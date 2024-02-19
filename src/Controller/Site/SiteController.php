c8a9b6ec8a6fb627cd2e7e8ed23c2e1ce2dba96c
Novitz Jean-Philippe <novitz@gmail.com>
2024-02-19 21:48:51 +0100

    New: Route site/show
    - Add route show to controller
    - Add show template

--- src/Controller/Site/SiteController.php
+++ src/Controller/Site/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Site;
 
+use App\Entity\Site;
 use App\Repository\SiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class SiteController extends AbstractController
 {
 
-    public function __construct(private SiteRepository $siteRepository)
-    {
-
-    }
-
     #[Route('/', name: 'app_site_index')]
     public function index(): Response
     {
         return $this->render('site/index.html.twig');
+    }
+
+    #[Route('/site/{slug}', name: 'app_site_show')]
+    public function show(Site $site): Response
+    {
+        return $this->render('site/show.html.twig', [
+            'site' => $site
+        ]);
     }
 }
