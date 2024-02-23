32e99d7e5a19a31b3741c8bd02b4b164aeb8600f
Novitz Jean-Philippe <novitz@gmail.com>
2024-02-23 05:54:44 +0100

    Refactor: Admin SiteControllerTest
    - Fix issues on multiple functions
    - Add Mock Image

--- tests/Controller/Admin/SiteControllerTest.php
+++ tests/Controller/Admin/SiteControllerTest.php
@@ -14,8 +14,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use org\bovigo\vfs\vfsStream;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class SiteControllerTest extends WebTestCase
 {
@@ -29,6 +31,8 @@ class SiteControllerTest extends WebTestCase
     private SiteRepository $siteRepository;
     private string $path = '/admin/site/';
 
+    private mixed $imageFile;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -39,6 +43,30 @@ class SiteControllerTest extends WebTestCase
         $this->siteRepository = $this->manager->getRepository(Site::class);
         $this->categoryRepository = $this->manager->getRepository(Category::class);
         $this->tagRepository = $this->manager->getRepository(Tag::class);
+
+//        $this->fileSystemMock = vfsStream::setup('root');
+
+        // file system for images
+//        $imageContent = file_get_contents(__DIR__ . '/../../../fixtures/test/image_test.jpg'); // Assurez-vous que ce fichier existe pour le test initial
+//        $virtualImagePath = vfsStream::url('root/image_test.jpg');
+//        file_put_contents($virtualImagePath, $imageContent);
+        $imagePath = __DIR__ . '/../../fixtures/image_test.jpg';
+
+        // Création d'un mock pour l'objet UploadedFile
+        $this->imageFile = $this->createMock(UploadedFile::class);
+
+        // Configuration du mock pour retourner des valeurs spécifiques
+        $this->imageFile->method('getClientOriginalName')->willReturn('image_test.jpg');
+        $this->imageFile->method('getClientMimeType')->willReturn('image/jpeg');
+        $this->imageFile->method('getError')->willReturn(UPLOAD_ERR_OK);
+        // Simuler le comportement de la méthode guessExtension si nécessaire
+        $this->imageFile->method('guessExtension')->willReturn('jpg');
+        // Simuler le comportement de moveTo pour éviter l'erreur de déplacement de fichier
+//        $this->imageFile->expects($this->once())->method('move')->with(
+//            $this->anything(), // Vous pouvez spécifier le chemin attendu si vous voulez être plus précis
+//            $this->equalTo('image_test.jpg')
+//        );
+
     }
 
     public function test_site_admin_page_index_is_not_accessible_anonymously(): void
@@ -77,7 +105,6 @@ class SiteControllerTest extends WebTestCase
         $site = $this->siteRepository->findAll()[0];
 
         $this->client->loginUser($user);
-        $this->client->request('GET', $this->path);
 
         $response = $this->client->request('GET', sprintf('%s%s', $this->path, $site->getId()));
 
@@ -106,7 +133,7 @@ class SiteControllerTest extends WebTestCase
         $this->client->submitForm('Save', [
             'site[title]' => 'Testing',
             'site[description]' => 'Testing',
-            'site[image]' => 'Testing',
+            'site[imageFile][file]' => $this->imageFile,
             'site[repo]' => 'Testing',
             'site[categories]' => [1],
             'site[tags]' => [1],
@@ -117,8 +144,6 @@ class SiteControllerTest extends WebTestCase
         self::assertSame(1, $this->siteRepository->count([]));
     }
 
-
-//
     public function test_site_admin_edit(): void
     {
         $this->databaseTool->loadAliceFixture([
@@ -129,37 +154,36 @@ class SiteControllerTest extends WebTestCase
 
 
         $user = $this->userRepository->findAll()[0];
+
         $this->client->loginUser($user);
 
         $fixture = new Site();
         $fixture->setTitle('Value');
         $fixture->setDescription('Value');
-        $fixture->setImage('Value');
+        $fixture->setImageFile($this->imageFile);
         $fixture->setRepo('Value');
         $fixture->addCategory($this->categoryRepository->findAll()[1]);
         $fixture->addTag($this->tagRepository->findAll()[0]);
+
         $this->manager->persist($fixture);
         $this->manager->flush();
 
-        $crawler = $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
-//
-//        $form = $crawler->selectButton('Update')->form();
-////        dd($form['site[categories][1]']-);
-//        $form['site[categories]'] = [1=>'2', 2=>'3'];
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
 
         $this->client->submitForm('Update', [
             'site[title]' => 'Something New',
             'site[description]' => 'Something New',
-            'site[image]' => 'Something New',
+            'site[imageFile][file]' => $this->imageFile,
             'site[repo]' => 'Something New',
-            'site[categories]' => [1=>'2', 2=>'3'],
+            'site[categories]' => [1 => '2', 2 => '3'],
             'site[tags]' => [1, 2],
         ]);
 
         self::assertResponseRedirects('/admin/site/');
 
         $site = $this->siteRepository->findAll()[0];
-
         $category_ids = array_map(function ($category) {
             return $category->getId();
         }, iterator_to_array($site->getCategories()));
@@ -170,7 +194,7 @@ class SiteControllerTest extends WebTestCase
 
         self::assertSame('Something New', $site->getTitle());
         self::assertSame('Something New', $site->getDescription());
-        self::assertSame('Something New', $site->getImage());
+        self::assertStringContainsString('image-test', $site->getImageName());
         self::assertSame('Something New', $site->getRepo());
         self::assertContains(2, $category_ids);
         self::assertContains(2, $tag_ids);
@@ -182,19 +206,28 @@ class SiteControllerTest extends WebTestCase
             'fixtures/test/user.yaml',
             'fixtures/test/category.yaml',
             'fixtures/test/tag.yaml',
-            'fixtures/test/site.yaml',
         ]);
 
         $user = $this->userRepository->findAll()[0];
         $this->client->loginUser($user);
 
-        $count_sites = $this->siteRepository->count([]);
-        $expected_count = $count_sites-1;
+        $fixture = new Site();
+        $fixture->setTitle('Value');
+        $fixture->setDescription('Value');
+        $fixture->setImageFile($this->imageFile);
+        $fixture->setRepo('Value');
+        $fixture->addCategory($this->categoryRepository->findAll()[1]);
+        $fixture->addTag($this->tagRepository->findAll()[0]);
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
         $site_to_remove = $this->siteRepository->findAll()[0];
-        $this->client->request('GET', sprintf('%s%s', $this->path, $site_to_remove->getId()));
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $site_to_remove->getId()));
         $this->client->submitForm('Delete');
 
         self::assertResponseRedirects('/admin/site/');
-        self::assertSame($expected_count, $this->siteRepository->count([]));
+        self::assertSame(0, $this->siteRepository->count([]));
     }
 }
