925849e2f6435228b6f3d865c3a7e50bc917bee3
Novitz Jean-Philippe <novitz@gmail.com>
2024-02-25 00:14:58 +0100

    Refactor: tailwind config
    Add font weight to components

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,12 +23,15 @@ module.exports = {
             addBase({
                 'h1': {
                     fontSize: theme('fontSize.3xl'),
+                    fontWeight: theme('fontWeight.black')
                 },
                 'h2': {
                     fontSize: theme('fontSize.2xl'),
+                    fontWeight: theme('fontWeight.bold')
                 },
                 'h3': {
                     fontSize: theme('fontSize.xl'),
+                    fontWeight: theme('fontWeight.medium')
                 },
                 'h4': {
                     fontSize: theme('fontSize.lg'),
