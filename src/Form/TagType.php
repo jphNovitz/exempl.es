09507c1dbdacc7f5aa9beeebf2a72e40f3b30d83
Novitz Jean-Philippe <novitz@gmail.com>
2024-04-07 01:48:19 +0200

    Add Tag
    - TagType
    - Tag _form form partial
    - edit template
    - new template

---
+++ src/Form/TagType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use App\Entity\Site;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TagType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+//            ->add('tagGroup', EntityType::class, [
+//                'class' => Group::class,
+//                'choice_label' => 'id',
+//            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tag::class,
+        ]);
+    }
+}
