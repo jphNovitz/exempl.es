32a4275d860d425abcc535cc4cd2cb1f958e5c9c
Novitz Jean-Philippe <novitz@gmail.com>
2024-05-19 00:47:09 +0200

    Add sitepam

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,15 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+
 
 class DefaultController extends AbstractController
 {
     #[Route('/default', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('default/index.html.twig', []);
     }
+
+    #[Route('/sitemap', name: 'app_sitemap')]
+    public function sitemap(): Response
+    {
+        $response = new Response($this->render('default/sitemap.xml.twig'));
+        $response->headers->set('Content-Type', 'application/xml');
+        return  $response;
+    }
+
+
 }
